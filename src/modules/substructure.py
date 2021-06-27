bdeb92a61bfa63499704640ab582dc4a32c90a3d
Web Development <deve.publicmail@gmail.com>
2021-06-27 17:10:14 +0100

    renamed softstructure to substructure

--- src/modules/substructure.py
+++ src/modules/substructure.py
@@ -1,6 +1,198 @@
-class Super:
-    def getSuperInit(self):
-        pass
+class Sub:
+    def getSoftInit():
+        global bldLen, bldWid, spread, blockThickness, girthBIF
+        print("Please initialize the required values before you continue\n")
+        bldLen = float(input(("Enter the length of the building: ")))
+        bldWid = float(input("Enter the width of the building: "))
+        spread = float(input("Enter the foundation spread: "))
+        blockThickness = float(input("Enter the thickness of blockwork in foundation: "))
+        girthBIF = float(input('Enter the girth of blockwork in foundation: '))
+
+    def siteClearance():
+        print("Calculating for Site Clearance")
+        siteClr = ((bldLen+2) * (bldWid+2))
+        print(siteClr)
+        return siteClr
+
+    def topSoilExcavation():
+        global tsExcav
+        print("Calculating for Top Soil Excavation")
+        tsExcav = ((bldLen+(2*spread)) * (bldWid+(2*spread)))
+        print(tsExcav)
+        return tsExcav
+
+    # Top soil removal
+
+    def remOfTopSoilExcav():
+        global remOfTSE
+        remOfTSE = tsExcav * 0.15
+        print(remOfTSE)
+        return remOfTSE
+
+    # Trench Excavation
+
+    def trenchExcavation():
+        global trenchExcav, depthOfFdn, widOfFdn
+        widOfFdn = float(input("Enter the width of the foundation: "))
+        depthOfFdn = float(input("Enter the depth of the foundation: "))
+        trenchExcav = (girthBIF * widOfFdn * depthOfFdn)
+        print(trenchExcav)
+        return trenchExcav, depthOfFdn
+
+    # Leveling and compacting
+
+    def levAndCompacting():
+        global levAndComp
+        levAndComp = girthBIF * widOfFdn
+        print(levAndComp)
+        return levAndComp
+
+    # Concrete Footing
+
+    def concFooting():
+        global concFT, ftThickness
+        ftThickness = float(input("Enter the thickness of the footing: "))
+        concFT = girthBIF * widOfFdn * ftThickness
+        concFT = round(concFT, 2)
+        print(concFT)
+        return concFT, ftThickness
+
+    # Blockwork in foundation
+
+    def blockWorkInFDN():
+        global blockWInFDN, expVal
+        blckWkHeight = float(input("Enter the height of blockwork in foundation: "))
+        blockWInFDN = girthBIF * blckWkHeight * blockThickness
+        expVal = girthBIF * blckWkHeight  # Value to be exported
+        print(blockWInFDN)
+        return blockWInFDN, expVal
+
+    # Backfilling
+
+    def backfilling():
+        global bckFilling, remSurplusOffsite
+        blockWInFDN2 = girthBIF * (depthOfFdn - ftThickness) * blockThickness
+        remSurplusOffsite = blockWInFDN2 + concFT
+        bckFilling = trenchExcav - remSurplusOffsite
+        print(bckFilling)
+        # Export remSurplusOffsite in Removal of Surplus Offsite
+        return bckFilling, remSurplusOffsite
+
+    # Removal of surplus offsite
+
+    def remofSurpOffsite():
+        global remSurplusOffsite
+        remSurplusOffsite
+        print(remSurplusOffsite)
+        return remSurplusOffsite
+
+    # Laterite Earth Filling
+
+    def latEarthFilling():
+        global latEarthFill, fillingArea, areaOfBld, peremOfWall
+        thicknessOfLatFillling = float(
+            input("Enter the the thickness of laterite earth filling: "))
+        areaOfBld = bldLen*bldWid
+        peremOfWall = girthBIF * blockThickness
+        fillingArea = (areaOfBld - allRcsValues - peremOfWall)
+        latEarthFill = fillingArea * thicknessOfLatFillling
+        latEarthFill = round(latEarthFill, 2)
+        print(latEarthFill)
+        return latEarthFill, fillingArea, areaOfBld, peremOfWall
+
+    # Hardcore filling
+
+    def hardcoreFilling():
+        global hardcoreFill
+        thicknessOfCoreFillling = float(
+            input("Enter the thickness of hardcore filling: "))
+        hardcoreFill = fillingArea * thicknessOfCoreFillling
+        print(hardcoreFill)
+        return hardcoreFill
+
+    # Floor reinforcement
+
+    def floorReinforcement():
+        global floorReinf
+        floorReinf = areaOfBld - allRcsValues
+        print(floorReinf)
+        return floorReinf
+
+    # Damp proof course
+
+    def dampProofCourse():
+        global peremOfWall
+        PDC = peremOfWall
+        print(peremOfWall)
+        return peremOfWall
+
+    # Damp proof membrane
+
+    def dampProofMembrane():
+        global DPM
+        DPM = floorReinf
+        print(DPM)
+        return DPM
+
+    # Leveling and compacting on surface of filling
+
+    def surfaceLevelingAndCompacting():
+        global surfLvlAndComp
+        surfLvlAndComp = fillingArea
+        print(surfLvlAndComp)
+        return surfLvlAndComp
+
+    # Oversite concrete
+
+    def oversiteConcrete():
+        global oversiteConc
+        floorThickness = float(input("Enter the floor thickness: "))
+        oversiteConc = floorReinf * floorThickness
+        print(oversiteConc)
+        return oversiteConc
+
+    # Formwork for oversite concrete
+
+    def oversiteConcreteFormwork():
+        global oversiteConcFormwork
+        bldVal = (bldLen * 2) + (bldWid * 2)
+        oversiteConcFormwork = bldVal * floorThickness
+        print(oversiteConcFormwork)
+        return oversiteConcFormwork
+
+    def getRecessValue():
+        global allRcsValues
+        rcsTypes = input("How many type recess is present: ")
+        rcsLengths = []
+        rcsWidths = []
+        rcsUnits = []
+        rcsValues = []
+        rcsTypes = int(rcsTypes)
+        answer = 0
+        for i in range(1):
+            # Loop through to collect the lenght values
+            for rcsLen in range(rcsTypes):
+                rcsLenVal = float(input(
+                    "Enter the lenght for recess type %s : " % (rcsLen+1)))
+                rcsLengths.append((rcsLenVal))
+
+            for rcsWid in range(rcsTypes):
+                rcsWidVal = float(input(
+                    "Enter the width for recess type %s : " % (rcsWid+1)))
+                rcsWidths.append(rcsWidVal)
+
+            for rcsUnit in range(rcsTypes):
+                rcsUnitVal = float(input(
+                    "How many unit of recess type %s : " % (rcsUnit+1)))
+                rcsUnits.append(rcsUnitVal)
+
+            for value in range(rcsTypes):
+                rcsValue = (
+                    rcsLengths[value] * rcsWidths[value] * rcsUnits[value])
+                rcsValues.append(rcsValue)
+        allRcsValues = sum(rcsValues)
+        print("Total recesses value is %s" % allRcsValues)
+        return allRcsValues
 
     def getRingsAndBarDia():
         global weightOfBars, weightOfRings
@@ -316,12 +508,14 @@ class Super:
 
     def getWallThickness():
         global wiIF, wtAF, lenghtOfRings
-        wtIF = float(input("Enter the wall thickness in foundation (in meter): "))
+        wtIF = blockThickness
         wtAF = float(input("Enter the wall thickness above foundation (in meter): "))
         if wtAF == 0.23:
             lenghtOfRings = 0.82
-        else:
+        elif wtAF == 0.15:
             lenghtOfRings = 0.54
+        else:
+            lenghtOfRings = 0.82
 
         print(wtIF, wtAF, lenghtOfRings)
         return wtIF, wtAF, lenghtOfRings
--- src/take_off_calculator.py
+++ src/take_off_calculator.py
@@ -1,106 +1,146 @@
 #!/usr/bin/python3
 import sys
-from modules.softstructure import Soft
-from modules.superstructure import *
+import time
+import os
+from modules.substructure import Sub
+from modules.superstructure import Super
 
-Soft.getInit()
-Soft.getRecessValue()
+try:
+    print("Creating folder for take off report")
+    time.sleep((1.5))
+    os.mkdir('REPORTS')
+    print("Created the folder 'REPORTS' ")
+except FileExistsError:
+    print("REPORTS folder already exists")
 
+try:
+    Sub.getSoftInit()
+    Sub.getRecessValue()
+    Sub.getRingsAndBarDia()
+    Sub.getWallThickness()
+    Sub.getWindowsValue()
+    Sub.getDoorsValue()
+    Sub.getOtherOpeningsValue()
 
-# Global initializations
-# wtIF = float(input("Enter the wall thickness in foundation (in meter): "))
-# wtAF = float(input("Enter the wall thickness above foundation (in meter): "))
-# blockThickness = float(input("Enter the thickness for block: "))
-# girthBIF = int(input('Enter the girth of blockwork in foundation: '))
-# girthBAF = int(input("Enter girth for blockwork above foundation: "))
+    print('''
+    1.  Site Clearance
+    2.  Top Soil Excavation
+    3.  Removal of Top Soil Excavation
+    4.  Trench Excavation
+    5.  Leveling and Compaction on Trench
+    6.  Concreate Footing
+    7.  Blockwork in Foundation
+    8.  Backfilling 
+    9.  Removal of Surplus Offsite 
+    10. Laterite Earth Filling 
+    11. Hardcore Filling
+    12. Floor Reinforcement
+    13. Damp Proof Course
+    14. Damp Proof Membrane
+    15. Leveling and Compacting on Surface of Filling
+    16. Oversite Concrete
+    17. Formwork for Oversite Concrete
+    18. Concrete in Lintel
+    19. Reiforcement in Lintel
+    20. Formwork in Lintel
+    21.  Plastering/Rendering
+    22. Painting
+    23. Floor Finishes
+    24. Ceiling Finishes
+    25. Window Revel
+    ''')
 
+    status = True
+    while status:
+        userChoice = input("Please enter the value that corresponds with what you want to calculate: ")
+        userChoice = str(userChoice)
+        if userChoice == "Q":
 
-print('''
-1.  Site Clearance
-2.  Top Soil Excavation
-3.  Removal of Top Soil Excavation
-4.  Trench Excavation
-5.  Leveling and Compaction on Trench
-6.  Concreate Footing
-7.  Blockwork in Foundation
-8.  Backfilling 
-9.  Removal of Surplus Offsite 
-10. Laterite Earth Filling 
-11. Hardcore Filling
-12. Floor Reinforcement
-13. Damp Proof Course
-14. Damp Proof Membrane
-15. Leveling and Compacting on Surface of Filling
-16. Oversite Concrete
-17. Formwork for Oversite Concrete
-''')
+            print("Stopping program")
+            print(userChoice)
+            status = False
+            sys.exit()
 
-status = True
-while status:
-    userChoice = input("Please enter the value that corresponds with what you want to calculate: ")
-    userChoice = str(userChoice)
-    if userChoice == "Q":
+        elif userChoice == '1':
+            Soft.siteClearance()
 
-        print("Stopping program")
-        print(userChoice)
-        status = False
-        sys.exit()
+        elif userChoice == '2':
+            Soft.topSoilExcavation()
 
-    elif userChoice == '1':
-        Soft.siteClearance()
+        elif userChoice == '3':
+            Soft.remOfTopSoilExcav()
 
-    elif userChoice == '2':
-        Soft.topSoilExcavation()
+        elif userChoice == '4':
+            Soft.trenchExcavation()
 
-    elif userChoice == '3':
-        Soft.remOfTopSoilExcav()
+        elif userChoice == '5':
+            Soft.levAndCompacting()
 
-    elif userChoice == '4':
-        Soft.trenchExcavation()
+        elif userChoice == '6':
+            Soft.concFooting()
 
-    elif userChoice == '5':
-        Soft.levAndCompacting()
+        elif userChoice == '7':
+            Soft.blockWorkInFDN()
 
-    elif userChoice == '6':
-        Soft.concFooting()
+        elif userChoice == '8':
+            Soft.backfilling()
 
-    elif userChoice == '7':
-        Soft.blockWorkInFDN()
+        elif userChoice == '9':
+            Soft.remofSurpOffsite()
 
-    elif userChoice == '8':
-        Soft.backfilling()
+        elif userChoice == '10':
+            Soft.latEarthFilling()
 
-    elif userChoice == '9':
-        Soft.remofSurpOffsite()
+        elif userChoice == '11':
+            Soft.hardcoreFilling()
 
-    elif userChoice == '10':
-        Soft.latEarthFilling()
+        elif userChoice == '12':
+            Soft.floorReinforcement()
 
-    elif userChoice == '11':
-        Soft.hardcoreFilling()
+        elif userChoice == '13':
+            Soft.dampProofCourse()
 
-    elif userChoice == '12':
-        Soft.floorReinforcement()
+        elif userChoice == '14':
+            Soft.dampProofMembrane()
 
-    elif userChoice == '13':
-        Soft.dampProofCourse()
+        elif userChoice == '15':
+            Soft.surfaceLevelingAndCompacting()
 
-    elif userChoice == '14':
-        Soft.dampProofMembrane()
+        elif userChoice == '16':
+            Soft.oversiteConcrete()
 
-    elif userChoice == '15':
-        Soft.surfaceLevelingAndCompacting()
+        elif userChoice == '17':
+            Soft.oversiteConcreteFormwork()
 
-    elif userChoice == '16':
-        Soft.oversiteConcrete()
+    # SUPERSTRUCTURE
+        elif userChoice == '18':
+            Super.concInLintel()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
+        elif userChoice == '16':
+            Soft.latEarthFilling()
 
-    elif userChoice == '17':
-        Soft.oversiteConcreteFormwork()
-
-
-# SUPERSTRUCTURE
-    elif userChoice == '16':
-        Soft.latEarthFilling()
-
-    else:
-        print("Invalid Input")
+        else:
+            print("Invalid Input")
+except KeyboardInterrupt:
+    print("\nProgram terminated by user")
